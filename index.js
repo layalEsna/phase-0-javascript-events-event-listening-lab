aa63af76e1bcda9859d24e0dee64c13593633f40
Amene Esnaashari <layla_1380@yahoo.com>
2024-01-09 17:36:30 -0600

    eventlistener

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+function addingEventListener(){
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+      }); 
 }
+
+
+
